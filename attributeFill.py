19811f37d38b17d78af29f72ee7c116a6ae94c81
Sureshmdhr <sureshmdhr@gmail.com>
2020-11-20 12:06:15 +0545

    grid sheet code print for free sheet

--- attributeFill.py
+++ attributeFill.py
@@ -1,52 +1,55 @@
 from Tkinter import *
 
 version = "v2.1.1"
+ka_kha_ga = {"": "00",
+
+             "ka": "01",
+             "k": "01",
+
+             "kha": "02",
+             "kh": "02",
+
+             "ga": "03",
+             "gha": "04",
+             "nga": "05",
+             "cha": "06",
+             "chha": "07",
+             "ja": "08",
+             "jha": "09",
+             "yna": "10",
+             "Ta": "11",
+             "Tha": "12",
+             "Da": "13",
+             "Dha": "14",
+             "ana": "15",
+             "ta": "16",
+             "tha": "17",
+             "da": "18",
+             "dha": "19",
+             "na": "20",
+             "pa": "21",
+             "fa": "22",
+             "ba": "23",
+             "bha": "24",
+             "ma": "25",
+             "ya": "26",
+             "ra": "27",
+             "la": "28",
+             "wa": "29",
+             "sha": "30",
+             "SHA": "31",
+             "sa": "32",
+             "ha": "33",
+             "kshya": "34",
+             "tra": "35",
+             "gya": "36"
+             }
+
 
 class App(Frame):
     global version
-    ka_kha_ga={"":"00",
+    global ka_kha_ga
 
-               "ka":"01",
-               "k":"01",
-
-               "kha":"02",
-               "kh":"02",
-               
-               "ga":"03",
-               "gha":"04",
-               "nga":"05",
-               "cha":"06",
-               "chha":"07",
-               "ja":"08",
-               "jha":"09",
-               "yna":"10",
-               "Ta":"11",
-               "Tha":"12",
-               "Da":"13",
-               "Dha":"14",
-               "ana":"15",
-               "ta":"16",
-               "tha":"17",
-               "da":"18",
-               "dha":"19",
-               "na":"20",
-               "pa":"21",
-               "fa":"22",
-               "ba":"23",
-               "bha":"24",
-               "ma":"25",
-               "ya":"26",
-               "ra":"27",
-               "la":"28",
-               "wa":"29",
-               "sha":"30",
-               "SHA":"31",
-               "sa":"32",
-               "ha":"33",
-               "kshya":"34",
-               "tra":"35",
-               "gya":"36"
-               }
     def __init__(self, master):
         Frame.__init__(self, master)
         self.pack()
@@ -101,6 +104,12 @@ For recent file check https://github.com/neogeomat/SaexDataCleanUpScripts"""
                     print(x)
                     if x:
                         print(filename+","+x[0][0]+x[0][1]+x[0][2])
+                        bad_chars = ['_', '-', '(', ")"," "]
+                        new_string_name = ''.join(i for i in x[0][1] if not i in bad_chars)
+                        new_string_no = x[0][2]
+                        if new_string_no=="":
+                            new_string_no="0"
+                        print("code=,5555"+x[0][0].zfill(2)+ka_kha_ga[new_string_name]+new_string_no)
                         allerror.write (filename+","+x[0][0]+","+x[0][1]+","+x[0][2]+"\n")
                     else:
                         print(filename + "," + " ")
