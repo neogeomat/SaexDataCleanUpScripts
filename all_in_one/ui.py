445b10a0c35fbe96ca4f1f24594d39005a5f7e59
Suresh Manandhar <sureshmdhr@gmail.com>
2024-08-27 15:49:03 +0545

    Added checkbutton to run multiple buttons at once

--- all_in_one/ui.py
+++ all_in_one/ui.py
@@ -9,6 +9,7 @@ from Generalize import Generalize
 from Recalculate_Extent import recalculate_extent
 from Remove_Identical_Feature import Remove_Identical_Feature
 from Repair_Layers_Geometry import Repair_Geometry
+from merge_dummy_planning import merge_dummy_planning
 from mege_saex_mdb import mergeSaexMdbs
 from attribute_check import attributeChecker
 from file_filter import FileFilter  # Import the filter class
@@ -16,6 +17,7 @@ from filter_dialog import FilterDialog, show_filter_list  # Import the filter di
 from attributeFill_ward_grid import Fill_Ward_Grid
 from attributeFill_VDC_dist_code import Fill_VDC_Dist_Code
 from Fill_FID import Fill_Par_FID
+from tkinter import *
 
 class DataCleanup:
     def __init__(self, master):
@@ -24,65 +26,94 @@ class DataCleanup:
         self.create_widgets()
 
     def create_widgets(self):
-        """Create buttons that do nothing"""
+        """Create buttons with light, colorful background colors"""
+
+        # Define colors
+        colors = {
+            "light_blue": "#add8e6",
+            "check_button": "#add8e6",
+            "light_green": "#90ee90",
+            "light_yellow": "#ffffe0",
+            "light_coral": "#f08080",
+            "light_pink": "#ffb6c1",
+            "light_gray": "#d3d3d3",
+            "white": "#ffffff"
+        }
 
         # Section for loading and filtering files
-        file_section = LabelFrame(self.master, text="Choose Path and Filter", padx=5, pady=5)
-        file_section.grid(row=0, column=0, padx=10, pady=10, sticky=E + W + N + S, columnspan=3)
+        file_section = LabelFrame(self.master, text="Choose Path and Filter", padx=5, pady=5, bg=colors["light_blue"])
+        file_section.grid(row=0, column=0, padx=10, pady=10, sticky=E + W + N + S, columnspan=2)
 
-        # create label for sheet
-        self.Sheet = Label(file_section, text="Choose Folder", width=30)
+        # Create label for sheet
+        self.Sheet = Label(file_section, text="Choose Folder", width=30, bg=colors["light_gray"])
         self.Sheet.grid(row=0, column=0, padx=5, pady=5, sticky=E + W + N + S)
 
-        # create entry.
-        self.directory = Entry(file_section, width=30)
+        # Create entry
+        self.directory = Entry(file_section, width=30, bg=colors["white"])
         self.directory.grid(row=0, column=1, padx=5, pady=5, sticky=E + W + N + S)
 
-        self.browseDb = Button(file_section, text="Browse", command=self.browse_folder, width=30)
+        self.browseDb = Button(file_section, text="Browse", command=self.browse_folder, width=30, bg=colors["light_coral"])
         self.browseDb.grid(row=0, column=2, padx=5, pady=5, sticky=E + W + N + S)
 
-        self.loaddb = Button(file_section, text="Load DB", command=self.load_db, width=30)
+        self.loaddb = Button(file_section, text="Load DB", command=self.load_db, width=45, bg=colors["light_coral"])
         self.loaddb.grid(row=1, column=0, padx=5, pady=5, sticky=E + W + N + S, columnspan=3)
 
-        self.filter_button = Button(file_section, text="Filter Files", command=self.open_filter_dialog, width=30)
-        self.filter_button.grid(row=2, column=0, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
+        self.filter_button = Button(file_section, text="Filter Files", command=self.open_filter_dialog, width=30, bg=colors["light_coral"])
+        self.filter_button.grid(row=2, column=0, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
 
-        self.show_filter = Button(file_section, text="Show Filtered Files", command=lambda: show_filter_list(self), width=30)
-        self.show_filter.grid(row=2, column=1, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
+        self.show_filter = Button(file_section, text="Show Filtered Files", command=lambda: show_filter_list(self), width=30, bg=colors["light_coral"])
+        self.show_filter.grid(row=2, column=2, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
 
         # Section for database operations
-        cm_section = LabelFrame(self.master, text="Replace Whole MDb", padx=5, pady=5)
+        cm_section = LabelFrame(self.master, text="Replace Whole MDb", padx=5, pady=5, bg=colors["light_blue"])
         cm_section.grid(row=1, column=0, padx=10, pady=10, sticky=E + W + N + S, columnspan=3)
 
-        self.Sheet_cm = Label(cm_section, text="Choose Central Meridian", width=30)
-        self.Sheet_cm.grid(row=1, column=0, padx=5, pady=5, sticky=E + W + N + S)
+        self.Sheet_cm = Label(cm_section, text="Choose Central Meridian", width=30, bg=colors["light_gray"])
+        self.Sheet_cm.grid(row=1, column=0, padx=5, pady=5, sticky=E + W + N + S,columnspan=1)
 
         options_cm = [
             "BLANK.mdb",
-            "BLANK87.mdb",
+            "BLANK81.mdb",
             "BLANK84.mdb",
-            "BLANK81.mdb"
+            "BLANK87.mdb"
         ]
 
         self.variable_cm = StringVar(cm_section)
-        self.variable_cm.set(options_cm[1]) #default value
+        self.variable_cm.set(options_cm[1]) # Default value
         self.optionmenu_cm = OptionMenu(cm_section, self.variable_cm, *options_cm)
-        self.optionmenu_cm.grid(row=1, column=1, padx=5, pady=5, sticky=E + W + N + S)
+        self.optionmenu_cm.config(bg=colors["white"])
+        self.optionmenu_cm.grid(row=1, column=1, padx=5, pady=5, sticky=E + W + N + S,columnspan=1)
 
-        self.compactdb = Button(cm_section, text="Replace", command=lambda: replaceMDb(self,self.variable_cm.get()), width=30)
-        self.compactdb.grid(row=1, column=2, padx=5, pady=5, sticky=E + W + N + S, columnspan=3)
+        self.compactdb = Button(cm_section, text="Replace", command=lambda: replaceMDb(self, self.variable_cm.get()), width=30, bg=colors["light_coral"])
+        self.compactdb.grid(row=1, column=2, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
 
         # Section for database operations
-        db_section = LabelFrame(self.master, text="Apply Cleanup", padx=5, pady=5)
+        db_section = LabelFrame(self.master, text="Apply Cleanup", padx=5, pady=5, bg=colors["light_blue"])
         db_section.grid(row=2, column=0, padx=10, pady=10, sticky=E + W + N + S, columnspan=2)
 
-        self.compactdb = Button(db_section, text="Compact DB", command=lambda: compactDb(self), width=30)
+        # Store the checkbutton states
+        self.check_vars = {
+            "compactdb": BooleanVar(),
+            "attr_check": BooleanVar(),
+            "attr_fill1": BooleanVar(),
+            "attr_fill2": BooleanVar(),
+            "corr_fid": BooleanVar(),
+            "generalize": BooleanVar(),
+            "recalculate_extent": BooleanVar(),
+            "remove_identical": BooleanVar(),
+            "repair_geometry": BooleanVar()
+        }
+
+
+        self.compactdb = Button(db_section, text="Compact DB", command=lambda: compactDb(self), width=30, bg=colors["light_coral"])
         self.compactdb.grid(row=0, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section, bg=colors["check_button"],   variable=self.check_vars["compactdb"]).grid(row=0, column=5)
 
-        self.attr_check = Button(db_section, text="Check Attributes", command=lambda: attributeChecker(self), width=30)
+        self.attr_check = Button(db_section, text="Check Attributes", command=lambda: attributeChecker(self), width=30, bg=colors["light_coral"])
         self.attr_check.grid(row=1, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["attr_check"]).grid(row=1, column=5)
 
-        self.Sheet_sc = Label(db_section, text="Choose Mapped Scale", width=30)
+        self.Sheet_sc = Label(db_section, text="Choose Mapped Scale", width=30, bg=colors["light_gray"])
         self.Sheet_sc.grid(row=2, column=0, padx=5, pady=5, sticky=E + W + N + S)
 
         options_sc = [
@@ -96,62 +127,98 @@ class DataCleanup:
         ]
 
         self.variable_sc = StringVar(db_section)
-        self.variable_sc.set(options_sc[5]) #default value
+        self.variable_sc.set(options_sc[5]) # Default value
 
         self.optionmenu_sc = OptionMenu(db_section, self.variable_sc, *options_sc)
+        self.optionmenu_sc.config(bg=colors["white"])
         self.optionmenu_sc.grid(row=2, column=1, padx=5, pady=5, sticky=E + W + N + S)
 
-        self.attr_fill1 = Button(db_section, text="Fill Ward and Grid (Free)", command=lambda: Fill_Ward_Grid(self,self.variable_sc.get()), width=30)
+        self.attr_fill1 = Button(db_section, text="Fill Ward and Grid (Free)", command=lambda: Fill_Ward_Grid(self, self.variable_sc.get()), width=30, bg=colors["light_coral"])
         self.attr_fill1.grid(row=2, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["attr_fill1"]).grid(row=2, column=5)
 
-        # create label for District
-        self.Dist_code_label = Label(db_section, text="District Code", width=30)
+        # Create label for District
+        self.Dist_code_label = Label(db_section, text="District Code", width=30, bg=colors["light_gray"])
         self.Dist_code_label.grid(row=3, column=0, padx=5, pady=5, sticky=E + W + N + S)
 
-        # create entry.
-        self.DistrictCode = Entry(db_section, width=30)
+        # Create entry
+        self.DistrictCode = Entry(db_section, width=30, bg=colors["white"])
         self.DistrictCode.grid(row=3, column=1, padx=5, pady=5, sticky=E + W + N + S)
 
-        # create label for VDC
-        self.Vdc_code_label = Label(db_section, text="Enter VDC Code", width=30)
+        # Create label for VDC
+        self.Vdc_code_label = Label(db_section, text="Enter VDC Code", width=30, bg=colors["light_gray"])
         self.Vdc_code_label.grid(row=4, column=0, padx=5, pady=5, sticky=E + W + N + S)
 
-        # create entry.
-        self.VDCCode = Entry(db_section, width=30)
+        # Create entry
+        self.VDCCode = Entry(db_section, width=30, bg=colors["white"])
         self.VDCCode.grid(row=4, column=1, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
 
-        self.attr_fill2 = Button(db_section, text="Fill District VDC Code", command=lambda: Fill_VDC_Dist_Code(self,self.DistrictCode.get(),self.VDCCode.get()), width=30)
+        self.attr_fill2 = Button(db_section, text="Fill District VDC Code", command=lambda: Fill_VDC_Dist_Code(self, self.DistrictCode.get(), self.VDCCode.get()), width=30, bg=colors["light_coral"])
         self.attr_fill2.grid(row=4, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["attr_fill2"]).grid(row=4, column=5)
 
-        self.corr_fid = Button(db_section, text="Correct FID", command=self.Correct_FID, width=30)
+        self.corr_fid = Button(db_section, text="Correct FID", command=self.Correct_FID, width=30, bg=colors["light_coral"])
         self.corr_fid.grid(row=5, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["corr_fid"]).grid(row=5, column=5)
 
-        self.tolerance_label_text = Label(db_section, text="Tolerance(m)")
+        self.tolerance_label_text = Label(db_section, text="Tolerance(m)", bg=colors["light_gray"])
         self.tolerance_label_text.grid(row=6, column=0, sticky="e", padx=5, pady=2)
 
-        self.tolerance_entry = Entry(db_section)
+        self.tolerance_entry = Entry(db_section, bg=colors["white"])
         self.tolerance_entry.insert(0, "0.2")  # Insert default value
         self.tolerance_entry.grid(row=6, column=1, sticky="w", padx=5, pady=2)
 
-        self.generalize = Button(db_section, text="Generalize", command=lambda: Generalize(self,self.tolerance_entry.get()), width=30)
+        self.generalize = Button(db_section, text="Generalize", command=lambda: Generalize(self, self.tolerance_entry.get()), width=30, bg=colors["light_coral"])
         self.generalize.grid(row=6, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["generalize"]).grid(row=6, column=5)
 
-        self.recalculate_extent = Button(db_section, text="ReCalculate Extent", command=lambda: recalculate_extent(self), width=30)
+        self.recalculate_extent = Button(db_section, text="ReCalculate Extent", command=lambda: recalculate_extent(self), width=30, bg=colors["light_coral"])
         self.recalculate_extent.grid(row=7, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["recalculate_extent"]).grid(row=7, column=5)
 
-        self.remove_identical = Button(db_section, text="Remove Identical Constructions", command=lambda: Remove_Identical_Feature(self), width=30)
+        self.remove_identical = Button(db_section, text="Remove Identical Constructions", command=lambda: Remove_Identical_Feature(self), width=30, bg=colors["light_coral"])
         self.remove_identical.grid(row=8, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["remove_identical"]).grid(row=8, column=5)
 
-        self.repair_geometry = Button(db_section, text="Repair Geometry", command=lambda: Repair_Geometry(self), width=30)
+        self.repair_geometry = Button(db_section, text="Repair Geometry", command=lambda: Repair_Geometry(self), width=30, bg=colors["light_coral"])
         self.repair_geometry.grid(row=9, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        Checkbutton(db_section,bg=colors["check_button"],   variable=self.check_vars["repair_geometry"]).grid(row=9, column=5)
 
+        # Final button to run all checked commands
+        self.run_all_button = Button(db_section, text="Run Checked Actions", command=self.run_checked_actions, width=60, bg=colors["light_green"])
+        self.run_all_button.grid(row=10, column=0, padx=5, pady=5, sticky=E + W + N + S, columnspan=6)
 
-        # Section for database operations
-        extra_section = LabelFrame(self.master, text="Extras", padx=5, pady=5)
-        extra_section.grid(row=3, column=0, padx=10, pady=10, sticky=E + W + N + S)
+        # Section for extras
+        extra_section = LabelFrame(self.master, text="Extras", padx=5, pady=5, bg=colors["light_green"])
+        extra_section.grid(row=3, column=0, padx=10, pady=10, sticky=E + W + N + S,columnspan=3)
 
-        self.merge_all = Button(extra_section, text="Merge All", command=lambda: mergeSaexMdbs(self,self.variable_cm.get()), width=30)
-        self.merge_all.grid(row=0, column=3, padx=5, pady=5, sticky=E + W + N + S, columnspan=2)
+        self.merge_all = Button(extra_section, text="Merge All", command=lambda: mergeSaexMdbs(self, self.variable_cm.get()), width=30, bg=colors["light_coral"])
+        self.merge_all.grid(row=0, column=0, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
+
+        self.merge_dummy = Button(extra_section, text="Merge Dummy Planning", command=lambda: merge_dummy_planning(self, self.variable_cm.get()), width=30, bg=colors["light_coral"])
+        self.merge_dummy.grid(row=0, column=2, padx=5, pady=5, sticky=E + W + N + S, columnspan=1)
+
+    def run_checked_actions(self):
+        """Run all the commands associated with the checked checkbuttons."""
+        if self.check_vars["compactdb"].get():
+            compactDb(self)
+        if self.check_vars["attr_check"].get():
+            attributeChecker(self)
+        if self.check_vars["attr_fill1"].get():
+            Fill_Ward_Grid(self, self.variable_sc.get())
+        if self.check_vars["attr_fill2"].get():
+            Fill_VDC_Dist_Code(self, self.DistrictCode.get(), self.VDCCode.get())
+        if self.check_vars["corr_fid"].get():
+            self.Correct_FID()
+        if self.check_vars["generalize"].get():
+            Generalize(self, self.tolerance_entry.get())
+        if self.check_vars["recalculate_extent"].get():
+            recalculate_extent(self)
+        if self.check_vars["remove_identical"].get():
+            Remove_Identical_Feature(self)
+        if self.check_vars["repair_geometry"].get():
+            Repair_Geometry(self)
+
 
     def browse_folder(self):
         folder_selected = tkFileDialog.askdirectory()
